53c68bd5e1b63a8d8274cbc08a11e59f77a3ad98
Martin Hofmann-Sobik <martin.hofmannsobik@1und1.de>
2019-07-25 10:35:54 +0200

    add dot renderer

---
+++ src/main/kotlin/com/github/masooh/gocdpicodsl/DotRenderer.kt
@@ -0,0 +1,32 @@
+package com.github.masooh.gocdpicodsl
+
+import org.jgrapht.Graph
+import org.jgrapht.graph.DefaultEdge
+import java.io.StringWriter
+
+val PipelineSingle.dotName
+    get() = name.replace("-", "_")
+
+fun Graph<PipelineSingle, DefaultEdge>.toDot(plantUmlWrapper: Boolean = false): String {
+    val writer = StringWriter()
+    if (plantUmlWrapper) {
+        writer.appendln("@startuml")
+    }
+    writer.appendln("digraph Pipelines {")
+
+    graph.vertexSet().forEach { pipeline ->
+        writer.appendln("    ${pipeline.dotName} [label=\"${pipeline.dotName}\\n${pipeline.template?.name}\"];")
+    }
+
+    writer.appendln()
+
+    graph.edgeSet().forEach { edge ->
+        writer.appendln("    ${this.getEdgeSource(edge).dotName} -> ${graph.getEdgeTarget(edge).dotName};")
+    }
+
+    writer.appendln("}")
+    if (plantUmlWrapper) {
+        writer.appendln("@enduml")
+    }
+    return writer.toString()
+}
