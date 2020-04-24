733814efbb0aa9a047adf0a58b6c5d9da6f205ad
Martin Hofmann-Sobik <martin.hofmannsobik@1und1.de>
2020-04-24 11:24:54 +0200

    Add documentation for writing custom extensions

--- examples/src/main/kotlin/net/oneandone/gocd/picodsl/examples/registry/PipelineExtensions.kt
+++ examples/src/main/kotlin/net/oneandone/gocd/picodsl/examples/registry/PipelineExtensions.kt
@@ -1,10 +1,25 @@
 package net.oneandone.gocd.picodsl.examples.registry
 
-import net.oneandone.gocd.picodsl.dsl.PipelineGroup
-import net.oneandone.gocd.picodsl.dsl.PipelineSingle
+import net.oneandone.gocd.picodsl.dsl.*
 
-fun PipelineGroup.deploy(name: String, block: PipelineSingle.() -> Unit = {}) {
-    this.pipeline(name, block).apply {
+fun PipelineGroup.deploy(
+        name: String,
+        block: PipelineSingle.() -> Unit = {}
+) = deployPipeline(this, name, block)
+
+fun SequenceContext.deploy(
+        name: String,
+        block: PipelineSingle.() -> Unit = {}
+) = deployPipeline(this.pipelineGroup, name, block)
+
+fun ParallelContext.deploy(
+        name: String,
+        block: PipelineSingle.() -> Unit = {}
+) = deployPipeline(this.pipelineGroup, name, block)
+
+private fun deployPipeline(pipelineGroup: PipelineGroup, name: String, block: PipelineSingle.() -> Unit) {
+    pipelineGroup.pipeline(name, block).apply {
         template = deploy
+        parameter("param1", "value1")
     }
 }
